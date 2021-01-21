0122e22ce82cab263ddb21f4680c2f5989464e85
Djimmy Poliard <poliarddjimmy@gmail.com>
2021-01-20 20:14:04 -0500

    Created theme file

---
+++ utils/theme.js
@@ -0,0 +1,17 @@
+const theme = {
+  colors: {
+    black: '#010101',
+    antiqueBrass: '#cb9478',
+    tundora: '#434343',
+    apricotPeach: '#fbd7bb',
+    shipCove: '#7993bc',
+    nightshadz: '#ab3954',
+    gray: '#F4F4F4'
+  },
+  font: {
+    regular: "Inter_400Regular",
+    semiBold: "Inter_600SemiBold",
+  },
+};
+
+export default theme;
