fe761b2d53d977171c56f078cee788b004d634bd
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2021-10-13 15:54:17 -0400

    Getting ready to build a full-stack app with Prisma, Apollo Server, Next Auth and more

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
+import { SessionProvider } from "next-auth/react"
+
 import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 
-function TipToeApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function TipToeApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
 export default TipToeApp
