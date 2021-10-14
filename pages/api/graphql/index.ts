b77430dce0e0fbfc3b8da123f7047a67bd653ab8
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2021-10-14 01:04:33 -0400

    Prisman setup

--- pages/api/graphql/index.ts
+++ pages/api/graphql/index.ts
@@ -1,5 +1,20 @@
-const GraphQLAPI = () => {
+import { NextApiRequest, NextApiResponse } from "next"
+import { getSession } from "next-auth/react"
+import { prisma } from "../../../utils/clients"
+import { User } from '@prisma/client'
 
+type ResponseData = User[] | { error: string }
+
+const GraphQLAPI = async (req: NextApiRequest, res: NextApiResponse<ResponseData>) => {
+  const session = await getSession({ req })
+
+  if (!session) {
+    return res.status(401).json({ error: `You need to log in.` })
+  }
+
+  const data = await prisma.user.findMany()
+
+  return res.json(data)
 }
 
-export default GraphQLAPI;
+export default GraphQLAPI
